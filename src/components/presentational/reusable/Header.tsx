942e7e7a5697a35709f1c778838da9a68c69d562
Khoa Phan <khoaphan89@gmail.com>
2020-03-30 18:08:34 -0400

    Update Header styling

--- src/components/presentational/reusable/Header.tsx
+++ src/components/presentational/reusable/Header.tsx
@@ -3,7 +3,7 @@ import { Row, Col, Spinner } from 'reactstrap';
 
 const Header = ({ title, subtitle, inProgress }: { title: string; subtitle: string; inProgress: boolean }) => (
   <Row className="align-items-center">
-    <Col xs={6}>
+    <Col xs={10}>
       <div className="page-title-box">
         <h4 className="font-size-18">{title}</h4>
         <ol className="breadcrumb mb-0">
@@ -11,8 +11,8 @@ const Header = ({ title, subtitle, inProgress }: { title: string; subtitle: stri
         </ol>
       </div>
     </Col>
-    <Col xs={6}>
-      <div className="float-right">{inProgress && <Spinner size="large" color="warning" />}</div>
+    <Col xs={2}>
+      <div className="float-right mr-2">{inProgress && <Spinner size="large" type="grow" color="warning" />}</div>
     </Col>
   </Row>
 );
