9ec70d5cf6facc8363044379dd6e878303f7899e
Khoa Phan <kp@conclave.dev>
2020-04-18 15:52:40 +0000

    Add ledger hw u2f package and remove trailing slash from base derivation path

--- src/data/fetch/ledger.ts
+++ src/data/fetch/ledger.ts
@@ -1,7 +1,7 @@
 import { newLedgerWalletWithSetup } from '@celo/contractkit/lib/wallets/ledger-wallet';
 import TransportU2F from '@ledgerhq/hw-transport-u2f';
 
-const derivationPathBase = "44'/52752'/0'/0/";
+const derivationPathBase = "44'/52752'/0'/0";
 
 const setUpLedger = async (derivationPathIndex: number) => {
   const transport = await TransportU2F.create();
