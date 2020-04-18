2af31a2784ca4dc5fa626534d3d6511664e27b8e
Khoa Phan <kp@conclave.dev>
2020-04-18 15:53:14 +0000

    Update Twitter UX

--- src/components/presentational/ecosystem/home/Content.tsx
+++ src/components/presentational/ecosystem/home/Content.tsx
@@ -2,18 +2,20 @@ import React, { memo } from 'react';
 import { Row, Col, Card, CardHeader } from 'reactstrap';
 import SimpleBar from 'simplebar-react';
 
-const Content = ({ icon, children }: { icon: string; children: any }) => (
-  <Card>
-    <CardHeader style={{ backgroundColor: '#fff' }}>
-      <Row>
-        <Col xs={6}>
-          <i className={`mdi mdi-${icon}`} />
-        </Col>
-        <Col xs={6}>
-          <p className="text-right mb-0">@celoist</p>
-        </Col>
-      </Row>
-    </CardHeader>
+const Content = ({ icon, className, children }: { icon?: string; className?: string; children: any }) => (
+  <Card className={className}>
+    {icon && (
+      <CardHeader style={{ backgroundColor: '#fff' }}>
+        <Row>
+          <Col xs={6}>
+            <i className={`mdi mdi-${icon}`} />
+          </Col>
+          <Col xs={6}>
+            <p className="text-right mb-0">@celoist</p>
+          </Col>
+        </Row>
+      </CardHeader>
+    )}
     <SimpleBar style={{ maxHeight: '100vh' }}>{children}</SimpleBar>
   </Card>
 );
--- src/components/presentational/ecosystem/home/Twitter.tsx
+++ src/components/presentational/ecosystem/home/Twitter.tsx
@@ -1,11 +1,25 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import Content from './Content';
+import Spinner from '../../reusable/Spinner';
 
-const Twitter = () => (
-  <Content icon="twitter">
-    <TwitterTimelineEmbed sourceType="list" ownerScreenName="celoist" id="1242176827742982146" noHeader={false} noBorders />
-  </Content>
-);
+const Twitter = () => {
+  const [showOnLoad, setShowOnLoad] = useState('none');
+
+  return (
+    <>
+      {showOnLoad === 'none' && <Spinner />}
+      <Content className={`d-${showOnLoad}`}>
+        <TwitterTimelineEmbed
+          sourceType="list"
+          ownerScreenName="celoist"
+          id="1242176827742982146"
+          noHeader={false}
+          onLoad={() => setShowOnLoad('block')}
+        />
+      </Content>
+    </>
+  );
+};
 
 export default memo(Twitter);
