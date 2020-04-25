42f652921e50c78a7decf9f16ced2eb20ecdd3dc
Khoa Phan <kp@conclave.dev>
2020-04-25 15:10:56 +0000

    Resolve error related to renaming wallet file to account

--- src/components/presentational/navigation/TopbarWalletMenu.tsx
+++ src/components/presentational/navigation/TopbarWalletMenu.tsx
@@ -3,9 +3,9 @@ import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import { connect, ConnectedProps } from 'react-redux';
-import { connectLedger, disconnectLedger } from '../../../data/actions/wallets';
+import { connectLedger, disconnectLedger } from '../../../data/actions/account';
 
-const mapState = ({ wallets: { ledger, errorMessage } }) => ({ ledger, errorMessage });
+const mapState = ({ account: { ledger, errorMessage } }) => ({ ledger, errorMessage });
 const mapDispatch = { connectLedger, disconnectLedger };
 const connector = connect(mapState, mapDispatch);
 
