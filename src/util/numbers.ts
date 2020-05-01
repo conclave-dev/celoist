2e1577fc6e9bd61df05c9154b41ad0c7383a4d6d
Khoa Phan <kp@conclave.dev>
2020-05-01 18:20:40 +0000

    Add default n value to address undefined value errors

--- src/util/numbers.ts
+++ src/util/numbers.ts
@@ -18,11 +18,11 @@ const tokenBase = new BigNumber('1e18');
 
 const formatN = (n: number) => new BigNumber(n).toFormat(0);
 
-const formatSlashingMultiplier = (n: BigNumber) => n.dividedBy(slashingMultiplierBase).minus(1).toFormat(2);
-const formatCommission = (n: BigNumber) => n.dividedBy(commissionBase).toFormat(2);
-const formatVotes = (n: BigNumber) => n.dividedBy(voteBase).toFormat(0);
-const formatScore = (n: BigNumber) => n.dividedBy(scoreBase).toFormat(2);
-const formatTokens = (n: BigNumber) => n.dividedBy(tokenBase).toFormat(2);
+const formatSlashingMultiplier = (n = new BigNumber(0)) => n.dividedBy(slashingMultiplierBase).minus(1).toFormat(2);
+const formatCommission = (n = new BigNumber(0)) => n.dividedBy(commissionBase).toFormat(2);
+const formatVotes = (n = new BigNumber(0)) => n.dividedBy(voteBase).toFormat(0);
+const formatScore = (n = new BigNumber(0)) => n.dividedBy(scoreBase).toFormat(2);
+const formatTokens = (n = new BigNumber(0)) => n.dividedBy(tokenBase).toFormat(2);
 
 // Abbreviates long numbers into a more readable format
 export { formatN, formatSlashingMultiplier, formatCommission, formatVotes, formatScore, formatTokens };
