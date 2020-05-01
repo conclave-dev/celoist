4f924c2ef99e95f140a43cf01dcd28e8d1f6d043
Khoa Phan <kp@conclave.dev>
2020-05-01 21:55:38 +0000

    Add stage-filtering action and group proposals by stages

--- src/data/actions/governance.ts
+++ src/data/actions/governance.ts
@@ -1,4 +1,4 @@
-import { FETCH_PROPOSALS } from './actions';
+import { FETCH_PROPOSALS, FILTER_PROPOSALS_BY_STAGE } from './actions';
 import { handleInit, handleData, handleError } from '../util/actions';
 import { getGovernanceProposals } from '../fetch/governance';
 
@@ -7,21 +7,24 @@ const fetchProposals = () => async (dispatch) => {
 
   try {
     const {
-      queuedProposalsById,
-      dequeuedProposalsById,
-      allQueuedProposalIds,
-      allDequeuedProposalIds
+      queuedProposalsByStage,
+      dequeuedProposalsByStage,
+      allQueuedProposalStages,
+      allDequeuedProposalStages
     } = await getGovernanceProposals();
 
     return handleData(dispatch, FETCH_PROPOSALS, {
-      queuedProposalsById,
-      dequeuedProposalsById,
-      allQueuedProposalIds,
-      allDequeuedProposalIds
+      queuedProposalsByStage,
+      dequeuedProposalsByStage,
+      allQueuedProposalStages,
+      allDequeuedProposalStages
     });
   } catch (err) {
     return handleError(dispatch, FETCH_PROPOSALS, err);
   }
 };
 
-export { fetchProposals };
+const filterProposalsByStage = (stage: string) => async (dispatch) =>
+  handleData(dispatch, FILTER_PROPOSALS_BY_STAGE, { stageFilter: stage });
+
+export { fetchProposals, filterProposalsByStage };
