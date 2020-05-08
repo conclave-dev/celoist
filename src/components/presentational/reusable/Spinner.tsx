b9f13f52c8fa5088f7bfc91f5700db1c74cd3375
Khoa Phan <kp@conclave.dev>
2020-05-08 22:55:11 +0000

    Add optional color prop to Spinner

--- src/components/presentational/reusable/Spinner.tsx
+++ src/components/presentational/reusable/Spinner.tsx
@@ -1,9 +1,9 @@
 import React, { memo } from 'react';
 import { Spinner } from 'reactstrap';
 
-const ReusableSpinner = () => (
+const ReusableSpinner = ({ color }: { color?: string }) => (
   <div className="pt-4 pb-4 d-flex justify-content-center align-items-center">
-    <Spinner type="grow" color="warning" />
+    <Spinner type="grow" color={color || 'warning'} />
   </div>
 );
 
